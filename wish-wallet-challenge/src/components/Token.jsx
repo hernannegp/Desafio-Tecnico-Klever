6a2759b721e2c14a3cae6411f70d8b335bc81405
Hernanne Domiciano <hernannedomiciano@gmail.com>
2022-03-24 17:41:47 -0300

    feat: addToken page (complete)

--- wish-wallet-challenge/src/components/Token.jsx
+++ wish-wallet-challenge/src/components/Token.jsx
@@ -9,14 +9,34 @@ const Token = () => {
   const history = useNavigate();
 
   const [tokenInput, setTokenInput] = useState('');
+  const [balanceInput, setBalanceInput] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleSaveClick = (e) => {
     e.preventDefault();
-    const myArr = [tokenInput]
-    if (localStorage.length === 0) {
-      localStorage.setItem('tokens', JSON.stringify(myArr))
+    if (localStorage.getItem('tokens') === null) {
+      localStorage.setItem('tokens', '[]');
     }
-    const myTokens = JSON.parse(localStorage.getItem('tokens'))
+    if (localStorage.getItem('balances') === null) {
+      localStorage.setItem('balances', '[]');
+    }
+    if (tokenInput.length > 0 && balanceInput.length > 0) {
+      const myBalances = JSON.parse(localStorage.getItem('balances'));
+      const myTokens = JSON.parse(localStorage.getItem('tokens'));
+      myBalances.push(balanceInput);
+      myTokens.push(tokenInput);
+      localStorage.setItem('tokens', JSON.stringify(myTokens));
+      localStorage.setItem('balances', JSON.stringify(myBalances));
+      setIsVisible(false);
+      history('/');
+    }
+    if (!tokenInput && !balanceInput) {
+      setIsVisible(true)
+    }
+  }
+
+  const handleBackButton = () => {
+    history('/')
   }
 
   return (
@@ -26,7 +46,7 @@ const Token = () => {
       <form>
         <div className='addToken-title-and-button'>
           <h2 className='addToken-title'>Add Token</h2>
-          <button className="back-button">Voltar</button>
+          <button onClick={ handleBackButton } className="back-button">Voltar</button>
         </div>
         <div className='inputs-container'>
           <label htmlFor='inputToken'>
@@ -35,13 +55,14 @@ const Token = () => {
           </label>
           <label htmlFor='balanceToken'>
             <p className='balance-p'>Balance</p>
-            <input className='inputText' type='text' name='balanceToken'/>
+            <input onChange={(e) => setBalanceInput(e.target.value)} className='inputText' type='number' name='balanceToken'/>
           </label>
         </div>
           {
             pathname === '/editToken' ? <button className='remove-button'>Remove</button> : null
           }
           <button onClick={(e) => handleSaveClick(e)} className='save-button'>Save</button>
+          { isVisible === true ? <h1>Preencha os campos corretamente</h1> : null }
       </form>
     </TokenStyle>
   )
